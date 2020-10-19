f486608857de188d3ebee6280c0ee94669203feb
gloriango <gloriango12@gmail.com>
2020-10-19 20:33:02 +1100

    done! need to make it compliment when specific messages appear

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ComplimentDiscordBot
 A bot for discord which compliments people
+
+steps on how i did it ->
+1. put token into env file (make sure its in git ignore)
+2. npm install dotenv
+3. put require('dotenv').config(); at the top of the file which uses it 
+
+to get compliments 
+npm install grantcodes/complimenter
+
+
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+require('dotenv').config();
+
+var complimenter = require("complimenter");
+
+
 const Discord = require('discord.js');
+
+const botClient = new Discord.Client();
+
+const token = process.env.TOKEN
+botClient.on('ready', () => {
+    console.log("This bot is now online!");
+});
+ 
+botClient.on("message", msg => {
+    if(msg.content == "compliment me?"){
+        msg.reply(complimenter());
+    };
+});
+
+botClient.login(token)
